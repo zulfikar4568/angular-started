6ae2d4ecb4d695122c71f2366dbe00cf7700961a
zulfikar4568 <isnaen70@gmail.com>
2024-10-30 13:23:57 +0700

    feat: product details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { ProductAlertsComponent } from './product-alerts/product-alerts.componen
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
+    ProductDetailsComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Product, products } from '../data/products';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss']
+})
+export class ProductDetailsComponent {
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    this.product = products.find(x => x.id === productIdFromRoute);
+  }
+}
